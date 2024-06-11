799906c569aa913e56ba644c2bd5347c62ef46b7
oynauzrici <onur@trexoin.com>
2024-06-11 22:34:09 +0300

    fix api endpoint client component

--- app/api/polls/[id]/route.ts
+++ app/api/polls/[id]/route.ts
@@ -8,40 +8,12 @@ const pool = new Pool({
   }
 });
 
-export async function POST(request: NextRequest) {
-  const { question, options } = await request.json();
-  try {
-    const result = await pool.query(
-      'INSERT INTO polls (question) VALUES ($1) RETURNING id',
-      [question]
-    );
-    const pollId = result.rows[0].id;
-
-    const optionQueries = options.map((option: string) =>
-      pool.query('INSERT INTO options (poll_id, text) VALUES ($1, $2)', [pollId, option])
-    );
-    await Promise.all(optionQueries);
-
-    return NextResponse.json({ pollId }, { status: 201 });
-    
-  } catch (err) {
-    const error = err as Error;
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
-
 export async function GET(request: NextRequest) {
   const url = new URL(request.url);
   const id = url.pathname.split('/').pop(); // Get the poll ID from the URL
 
-  if (!id || id === 'polls') { // if the ID is not provided or it's the /polls endpoint
-    try {
-      const result = await pool.query('SELECT * FROM polls');
-      return NextResponse.json(result.rows, { status: 200 });
-    } catch (err) {
-      const error = err as Error;
-      return NextResponse.json({ error: error.message }, { status: 500 });
-    }
+  if (!id) {
+    return NextResponse.json({ error: 'ID not provided.' }, { status: 400 });
   }
 
   try {
